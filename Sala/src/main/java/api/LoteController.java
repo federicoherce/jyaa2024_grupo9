7f90d030607ee308b9e5f526efb7f0560ab53b13
Gian Quaranta <gianq7@gmail.com>
2024-07-23 18:46:13 -0300

    WIP loteRequest

--- Sala/src/main/java/api/LoteController.java
+++ Sala/src/main/java/api/LoteController.java
@@ -2,17 +2,25 @@ package api;
 
 import java.util.List;
 import org.json.JSONObject;
+
+import bd.Insumo;
 import bd.Lote;
 import dao.LoteDAO;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import requests.LoteRequest;
 
 @Path("/lotes")
 public class LoteController {
@@ -36,6 +44,20 @@ public class LoteController {
         }
         return Response.ok(lotes).build();
     }
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Operation(summary = "Creacion de un nuevo lote")
+	@ApiResponses(value = {
+	    @ApiResponse(responseCode = "201", description = "Lote creado correctamente",
+	        content = @Content(mediaType = "application/json",
+	        schema = @Schema(implementation = Lote.class))),
+	    @ApiResponse(responseCode = "409", description = "Conflicto de datos")
+	})
+    public Response crearLote(@Parameter(description = "Datos del lote", required = true) LoteRequest lote) {
+		Lote aux = new Lote(lote.getNombre(), lote.getCodigo(), lote.getFechaElaboracion(), lote.getCantidadProducida(), )
+	}
 	
 	
 }
