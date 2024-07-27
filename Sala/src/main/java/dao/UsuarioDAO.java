7d62a6a9390f3e7d0e0de330d70e66aedc270601
Gian Quaranta <gianq7@gmail.com>
2024-07-27 18:29:24 -0300

    fix en manejo de usuarios

--- Sala/src/main/java/dao/UsuarioDAO.java
+++ Sala/src/main/java/dao/UsuarioDAO.java
@@ -23,6 +23,15 @@ public class UsuarioDAO extends GenericDAOImpl<Usuario, Integer>{
         } 
     }
     
+    public Usuario findByEmail(String email) {
+    	try {
+    		return super.getEm().createQuery(
+    	            "SELECT e FROM Usuario e WHERE e.email = :email", Usuario.class)
+    	            .setParameter("email", email).getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        } 
+    }    
     
 	public Usuario login(String email, String password) {
         try {
