a5a9e23c5c624664f5a48918a3f2fca771ab8510
Gian Quaranta <gianq7@gmail.com>
2024-09-16 12:33:55 -0300

    new fix url

--- SalaApp/src/app/auth.interceptor.ts
+++ SalaApp/src/app/auth.interceptor.ts
@@ -9,7 +9,7 @@ export class AuthInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = localStorage.getItem('token');
-    console.log("AAAAAAA");
+    //console.log("AAAAAAA");
     if (token) {
       const cloned = req.clone({
         headers: req.headers.set('Authorization', `Bearer ${token}`)
